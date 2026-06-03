ff0398ede5a453c84bd9a7f9e86fb8dada9fe54a
Your Name <you@example.com>
2026-06-02 21:46:34 -0300

    feat: Validação

--- src/main/java/com/padoca/estoque/controller/ProdutoController.java
+++ src/main/java/com/padoca/estoque/controller/ProdutoController.java
@@ -1,3 +1,5 @@
+
+
 package com.padoca.estoque.controller;
 
 import java.util.List;
@@ -5,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,50 +18,76 @@ import com.padoca.estoque.model.Produto;
 import com.padoca.estoque.repository.ProdutoRepository;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
 
 @Controller
 public class ProdutoController {
 
-    private final PrincipalController principalController;
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    ProdutoController(PrincipalController principalController) {
-        this.principalController = principalController;
-    }
 
+    @GetMapping("/produtos/editar/{id}")
+    public String editarProduto(
+        HttpSession sessao, @PathVariable Long id, Model model){
+
+            if (sessao.getAttribute("funcionario") == null)
+                return "redirect:/login";
+
+            Produto produto = produtoRepository.findById(id).orElseThrow();
+            model.addAttribute("produto", produto);
+            return "produtos/editar";
+        }
+    
     @GetMapping("/produtos")
-    public String listarProdutos(HttpSession sessao, Model model, @RequestParam(required = false) String buscaProduto){
-        if (sessao.getAttribute("funcionario") ==null)
+    public String listarProdutos(
+        HttpSession sessao, 
+        Model model, 
+        @RequestParam(required = false) String buscaProduto) {
+            
+        if (sessao.getAttribute("funcionario") == null)
             return "redirect:/login";
 
         List<Produto> produtos;
-        if (buscaProduto != null && !buscaProduto.isBlank()){
-         produtos = produtoRepository.findByNomeContainingIgnoreCase(buscaProduto);
-        
-    }else{
-        produtos = produtoRepository.findAll();
-    }
+        if (buscaProduto != null && !buscaProduto.isBlank())
+            produtos = produtoRepository.findByNomeContainingIgnoreCase(buscaProduto);
+        else
+            produtos = produtoRepository.findAll();
+
         model.addAttribute("produtos", produtos);
-        return "produtos/lista";  
+
+        return "produtos/lista";
     }
 
-     @PostMapping("/produtos/salvar")
-     public String salvarProduto(HttpSession sessao, @ModelAttribute Produto produto){
+    @PostMapping("/produtos/salvar")
+    public String salvarProduto(
+        HttpSession sessao, 
+       @Valid @ModelAttribute Produto produto, BindingResult errosAcumulados,// Classe responsável por encontrar todos os erros de validação do produto
+        Model model 
+       ){
+            
         if (sessao.getAttribute("funcionario") == null)
             return "redirect:/login";
-           
+
+        if (errosAcumulados.hasErrors()){
+            model.addAttribute("produto", produto);
+            model.addAttribute("produtos", produtoRepository.findAll());
+            return "produtos/editar";
+        }
+        
         produtoRepository.save(produto);
 
-         return "redirect:/produtos";
-     
+        return "redirect:/produtos";
     }
     @PostMapping("/produtos/deletar/{id}")
-    public String deletarProduto(HttpSession sessao, @PathVariable Long id){ // @PathVariable para pegar um valor dinamico da URL, nesse caso o id do produto a ser deletado
+    public String deletarProduto(HttpSession sessao, @PathVariable Long id){
         if (sessao.getAttribute("funcionario") == null)
             return "redirect:/login";
 
         produtoRepository.deleteById(id);
         return "redirect:/produtos";
     }
+    
 }
+
--- src/main/java/com/padoca/estoque/model/Produto.java
+++ src/main/java/com/padoca/estoque/model/Produto.java
@@ -4,21 +4,37 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "O nome deve ser preenchido")
     private String nome;
+
     private String descricao;
+
+    @NotNull(message = "A quantidade deve ser informada")
+    @Min(value = 0, message = "A quantidade não pode ser negativa")
     private Integer quantidade;
+
+    @NotNull(message = "O estoque mínimo deve ser informado")
+    @Min(value = 0, message = "O estoque mínimo não pode ser negativo")
     private Integer estoqueMinimo;
+    
     private String categoria;
 
 
     public Long getId() {
         return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    
     }
     public String getNome() {
         return nome;
